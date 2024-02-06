150b72ba231683b1c7213dce4d05adcd3b9d9c6c
alonKir <155305767+alonKir@users.noreply.github.com>
2024-02-06 19:54:55 +0200

    commenr out -> DebugLog

--- Assets/Scripts/EventManger.cs
+++ Assets/Scripts/EventManger.cs
@@ -13,7 +13,7 @@ public class EventManager : MonoBehaviour
 	
 	public static void StartEndOfGame(int winner)
 	{
-		Debug.Log("Game Over animation started!");
+		//Debug.Log("Game Over animation started!");
 		GameOverAnimation?.Invoke(winner);
 	}
 }
